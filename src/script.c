80ea16da2ef9ef8421624ecea12e673eb831b1fe
petar.alilovic@gmail.com <petar.alilovic@gmail.com>
2014-03-07 19:51:23 +0100

    DHCP4o6 option fetching and passing to dhcpv6.script

--- src/script.c
+++ src/script.c
@@ -341,11 +341,13 @@ void script_call(const char *status)
 	size_t dns_len, search_len, custom_len, sntp_ip_len, ntp_ip_len, ntp_dns_len;
 	size_t sip_ip_len, sip_fqdn_len, aftr_name_len, cer_len, addr_len;
 	size_t s46_mapt_len, s46_mape_len, s46_lw_len, passthru_len;
+	size_t fos_len;
 
 	odhcp6c_expire();
 
 	struct in6_addr *addr = odhcp6c_get_state(STATE_SERVER_ADDR, &addr_len);
 	struct in6_addr *dns = odhcp6c_get_state(STATE_DNS, &dns_len);
+	struct in6_addr *fos = odhcp6c_get_state(STATE_DHCP4O6_SERVERS, &fos_len);
 	uint8_t *search = odhcp6c_get_state(STATE_SEARCH, &search_len);
 	uint8_t *custom = odhcp6c_get_state(STATE_CUSTOM_OPTS, &custom_len);
 	struct in6_addr *sntp = odhcp6c_get_state(STATE_SNTP_IP, &sntp_ip_len);
@@ -375,6 +377,7 @@ void script_call(const char *status)
 		ipv6_to_env("RDNSS", dns, dns_len / sizeof(*dns));
 		ipv6_to_env("SNTP_IP", sntp, sntp_ip_len / sizeof(*sntp));
 		ipv6_to_env("NTP_IP", ntp, ntp_ip_len / sizeof(*ntp));
+		ipv6_to_env("FOS", fos, fos_len / sizeof(*fos));
 		fqdn_to_env("NTP_FQDN", ntp_dns, ntp_dns_len);
 		ipv6_to_env("SIP_IP", sip, sip_ip_len / sizeof(*sip));
 		fqdn_to_env("DOMAINS", search, search_len);
