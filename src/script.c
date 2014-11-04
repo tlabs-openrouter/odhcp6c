30f045a50d8dfb6e008e12a05b2df5b56786c212
Kaspar Schleiser <kaspar@schleiser.de>
2014-11-04 15:44:54 +0100

    4o6-servers_in_opt_88_or_99

--- src/script.c
+++ src/script.c
@@ -397,7 +397,7 @@ void script_call(const char *status, int delay, bool resume)
 		size_t dns_len, search_len, custom_len, sntp_ip_len, ntp_ip_len, ntp_dns_len;
 		size_t sip_ip_len, sip_fqdn_len, aftr_name_len, cer_len, addr_len;
 		size_t s46_mapt_len, s46_mape_len, s46_lw_len, passthru_len;
-		size_t fos_len, ncs_fqdn_len;
+		size_t fos_88_len, fos_99_len, ncs_fqdn_len;
 
 		signal(SIGTERM, SIG_DFL);
 		if (delay > 0) {
@@ -407,7 +407,8 @@ void script_call(const char *status, int delay, bool resume)
 
 		struct in6_addr *addr = odhcp6c_get_state(STATE_SERVER_ADDR, &addr_len);
 		struct in6_addr *dns = odhcp6c_get_state(STATE_DNS, &dns_len);
-		struct in6_addr *fos = odhcp6c_get_state(STATE_DHCP4O6_SERVERS, &fos_len);
+		struct in6_addr *fos_88 = odhcp6c_get_state(STATE_DHCP4O6_SERVERS_88, &fos_88_len);
+		struct in6_addr *fos_99 = odhcp6c_get_state(STATE_DHCP4O6_SERVERS_99, &fos_99_len);
 		uint8_t *search = odhcp6c_get_state(STATE_SEARCH, &search_len);
 		uint8_t *custom = odhcp6c_get_state(STATE_CUSTOM_OPTS, &custom_len);
 		struct in6_addr *sntp = odhcp6c_get_state(STATE_SNTP_IP, &sntp_ip_len);
@@ -436,7 +437,8 @@ void script_call(const char *status, int delay, bool resume)
 		ipv6_to_env("RDNSS", dns, dns_len / sizeof(*dns));
 		ipv6_to_env("SNTP_IP", sntp, sntp_ip_len / sizeof(*sntp));
 		ipv6_to_env("NTP_IP", ntp, ntp_ip_len / sizeof(*ntp));
-		ipv6_to_env("DHCP4_O_DHCP6_SERVERS", fos, fos_len / sizeof(*fos));
+		ipv6_to_env("DHCP4_O_DHCP6_SERVERS_88", fos_88, fos_88_len / sizeof(*fos_88));
+		ipv6_to_env("DHCP4_O_DHCP6_SERVERS_99", fos_99, fos_99_len / sizeof(*fos_99));
 		fqdn_to_env("NTP_FQDN", ntp_dns, ntp_dns_len);
 		ipv6_to_env("SIP_IP", sip, sip_ip_len / sizeof(*sip));
 		fqdn_to_env("DOMAINS", search, search_len);
